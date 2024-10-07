1cb14d633e5b884a03a939111c9da75bd81dd002
ruwinikularathne <ruwinikularathne2000@gmail.com>
2024-10-07 22:55:42 +0530

    changes h2 content in top

--- app/page.tsx
+++ app/page.tsx
@@ -123,7 +123,8 @@ export default function Home() {
 
       {/* Mood Selection Buttons */}
       <section className="text-center mb-6 z-10">
-        <h2 className="text-lg font-semibold text-gray-700 mb-4">Select your mood to Generate a quote</h2>
+      <h2 className="text-lg font-semibold text-gray-700 mb-4">Do you want to fix your mood?</h2>
+        <h2 className="text-lg font-semibold text-gray-700 mb-4">Select your mood from here to Generate a quote</h2>
         <div className="flex space-x-4 justify-center">
           {Object.keys(moodQuotes).map((currentMood) => (
             <button
