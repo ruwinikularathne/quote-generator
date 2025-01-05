dce14ed694a6fa14b5efa33230cb85a2324706fa
ruwinikularathne <ruwinikularathne2000@gmail.com>
2025-01-05 16:40:29 +0530

    footer changed

--- app/page.tsx
+++ app/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
 
       {/* Footer (Optional) */}
       <footer className="text-center py-4 text-gray-500 z-10 mt-8">
-        <p>Have a nice day ❤</p>
+        <p>Have a good day ❤</p>
       </footer>
     </main>
   );
