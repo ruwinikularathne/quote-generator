aac398f352e76a25b2a8bf9ddd742f95fc309116
ruwinikularathne <ruwinikularathne2000@gmail.com>
2025-03-14 23:32:23 +0530

    text of footer changed

--- app/page.tsx
+++ app/page.tsx
@@ -155,7 +155,7 @@ export default function Home() {
         </div>
       </section>
       <footer className="text-center py-4 text-gray-500 z-10 mt-8">
-        <p>Have a nice day ❤</p>
+        <p>Have a good day ❤</p>
       </footer>
     </main>
   );
